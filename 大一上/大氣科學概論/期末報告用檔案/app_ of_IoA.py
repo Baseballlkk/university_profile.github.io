4138d73256cf2e29e8aaf44553be6dee4f58d35a
Baseballlkk <103667337+Baseballlkk@users.noreply.github.com>
2023-06-01 01:12:34 +0800

    Delete 大一上 directory

--- 大一上/大氣科學概論/期末報告用檔案/app_ of_IoA.py
+++
@@ -1,9 +0,0 @@
-# import function
-import IoAgeo as I
-
-# input the latitude 
-phi = input('Input the latitude:')
-phi = float(phi)
-
-# conduct the program 
-I.geostrophic(phi)
--- 大一上/大氣科學概論/期末報告用檔案/testapp.py
+++
@@ -1,7 +0,0 @@
-import test as t
-
-
-
-for i in range(1,91):
-    print(i)
-    t.testgeo(i)
